9bc5b1b4713b7095510e0620da4a7aa627baa5f1
Abdallah Jarrar <a.jarrar@boutiqaat.com>
2021-11-15 00:13:52 -0800

    add validation to order mapper

--- src/main/java/com/shopingcart/rest/services/restfullwebservices/order/OrderHeaderMapper.java
+++ src/main/java/com/shopingcart/rest/services/restfullwebservices/order/OrderHeaderMapper.java
@@ -1,11 +1,14 @@
 package com.shopingcart.rest.services.restfullwebservices.order;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface OrderHeaderMapper {
 
+    @Mapping(target = "cart.cartItems", ignore = true)
+    @Mapping(target = "cart.orderHeader", ignore = true)
     OrderHeaderModel entityToModel(OrderHeader entity);
 
     OrderHeader modelToEntity(OrderHeaderModel model);
