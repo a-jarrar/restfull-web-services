e9676b59f1ef21a6f1af8651266ae2c0e9adf2ed
Abdallah Jarrar <a.jarrar@boutiqaat.com>
2021-11-15 22:54:28 -0800

    add some validation on order header mapper

--- src/main/java/com/shopingcart/rest/services/restfullwebservices/order/OrderHeaderMapper.java
+++ src/main/java/com/shopingcart/rest/services/restfullwebservices/order/OrderHeaderMapper.java
@@ -1,15 +1,31 @@
 package com.shopingcart.rest.services.restfullwebservices.order;
 
+import com.shopingcart.rest.services.restfullwebservices.orderItem.OrderItem;
+import com.shopingcart.rest.services.restfullwebservices.orderItem.OrderItemModel;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface OrderHeaderMapper {
 
     @Mapping(target = "cart.cartItems", ignore = true)
     @Mapping(target = "cart.orderHeader", ignore = true)
+    @Mapping(target = "orderItemList", qualifiedByName = "OrderItemList")
     OrderHeaderModel entityToModel(OrderHeader entity);
+
+    @Named("OrderItemList")
+    @IterableMapping(qualifiedByName = "nonCyclicOrderItem")
+    List<OrderItemModel> OrderItemList(List<OrderItem> orderItem);
+
+    @Named("nonCyclicOrderItem")
+    @Mapping(target = "orderHeader", ignore = true)
+    @Mapping(target = "cartItem", ignore = true)
+    OrderItemModel nonCyclicOrderItem(OrderItem orderItem);
 
     OrderHeader modelToEntity(OrderHeaderModel model);
 
